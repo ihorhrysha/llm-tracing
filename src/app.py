a0aa92731ce7d46e5e317103cddf78d57af7c08a
Ihor Hrysha <ihor.hrysha@gmail.com>
2024-08-30 17:20:11 +0300

    metric and logs to app

--- src/app.py
+++ src/app.py
@@ -1,20 +1,41 @@
 from random import randint
-from flask import Flask
+from flask import Flask, request, jsonify
+import logging
 
 from opentelemetry import trace
-
-# Acquire a tracer
-tracer = trace.get_tracer("diceroller.tracer")
+from opentelemetry import metrics
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# custom tracer and meter
+tracer = trace.get_tracer("diceroller.tracer")
+meter = metrics.get_meter("diceroller.meter")
+roll_counter = meter.create_counter(
+    "dice.rolls",
+    description="The number of rolls by roll value",
+)
+
 @app.route("/rolldice")
 def roll_dice():
-    return str(roll())
+    def roll():
+        return randint(1, 6)
 
-def roll():
     # This creates a new span that's the child of the current one
-    with tracer.start_as_current_span("roll") as rollspan:
-        res = randint(1, 6)
-        rollspan.set_attribute("roll.value", res)
-        return res
+    with tracer.start_as_current_span("roll") as roll_span:
+        player = request.args.get('player', default = None, type = str)
+        result = str(roll())
+        roll_span.set_attribute("roll.value", result)
+        # This adds 1 to the counter for the given roll value
+        roll_counter.add(1, {"roll.value": result})
+        if player:
+            logger.warning("{} is rolling the dice: {}", player, result)
+        else:
+            logger.warning("Anonymous player is rolling the dice: %s", result)
+        return result
+    
+@app.route("/")
+def status():
+    return jsonify({"status": "alive"})
