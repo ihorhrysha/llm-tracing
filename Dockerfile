6647fda3d3170364c138fbc0ab5c07dafeb5ee4d
Ihor Hrysha <ihor.hrysha@gmail.com>
2024-09-01 13:11:52 +0300

    flask app

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,32 @@
-FROM python:3.12-slim-bookworm
-WORKDIR /devops-hobbies
-ENV FLASK_APP app.py
-COPY src/requirements.txt . 
-RUN pip3 install -r requirements.txt
-COPY src/app.py .
-CMD ["flask", "run", "--host=0.0.0.0", "--port=8080"]
+# https://github.com/open-telemetry/opentelemetry-demo/blob/main/src/recommendationservice/Dockerfile
+
+FROM python:3.12-slim-bookworm as base
+
+#
+# Fetch requirements
+#
+FROM base as builder
+RUN apt-get -qq update \
+    && apt-get install -y --no-install-recommends g++ \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /usr/src/app/
+COPY ./src/requirements.txt ./
+
+RUN pip install --upgrade pip
+RUN pip install --prefix="/reqs" -r requirements.txt
+
+#
+# Runtime
+#
+FROM base as runtime
+WORKDIR /usr/src/app/
+COPY --from=builder /reqs /usr/local
+COPY ./src/ ./
+
+
+# This doesn't work quite well
+# RUN opentelemetry-bootstrap -a install
+
+# EXPOSE 8080
+ENTRYPOINT ["flask", "--app", "app_flask.py", "run", "-p", "8080", "-h", "0.0.0.0"]
--- src/app.py
+++ src/app.py
@@ -1,14 +1,33 @@
-from random import randint
-from flask import Flask, request, jsonify
+# These are the necessary import declarations
 import logging
+from random import randint
 
 from opentelemetry import trace
 from opentelemetry import metrics
+from flask import Flask, request, jsonify
+from langchain_openai import ChatOpenAI
+from langchain_community.utilities import SQLDatabase
+
+from logger import config_logger
+from settings import AppSettings
+from qa_controller import QAController, QAService
+
+
+# Primitive dependency graph
+app_settings = AppSettings()
+# config_logger(min_log_level=app_settings.log_level)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=app_settings.openai_api_key)
+db = SQLDatabase.from_uri(app_settings.db_uri)
+
+qa_service = QAService(llm=llm, db=db)
+qa_controller = QAController(qa_service=qa_service)
+
+
 
 # custom tracer and meter
 tracer = trace.get_tracer("diceroller.tracer")
@@ -35,7 +54,22 @@ def roll_dice():
         else:
             logger.warning("Anonymous player is rolling the dice: %s", result)
         return result
-    
-@app.route("/")
-def status():
-    return jsonify({"status": "alive"})
+
+
+@app.route("/ask")
+def ask():
+    """
+    Returns question about data in DB
+
+    Examples:
+        How many employees are there?
+        http://localhost:8080/ask?question=How%20many%20employees%20are%20there?
+    """
+    question = request.args.get('question', default=None, type=str)
+    if not question:
+        return jsonify({"error": "Question parameter is required"}), 400
+
+    logger.info(f"Asking question about data in DB: {question}")
+    answer = qa_controller.answer(question)
+    logger.warning(f"Answer: {answer}")
+    return jsonify({"answer": answer})
